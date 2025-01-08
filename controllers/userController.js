248859ae0a9bbcf2a9e5062fa0adf61816a83f4c
Talat09 <talat181008312@gmail.com>
2025-01-08 16:05:17 +0600

    added vercel json file

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,21 @@ import User from "../models/User.js";
 import generateToken from "../utils/generateToken.js";
 // Register User
 export const registerUser = async (req, res) => {
-    const {
+  const {
+    fullName,
+    gender,
+    dateOfBirth,
+    email,
+    employeeID,
+    position,
+    password,
+  } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    const user = await User.create({
       fullName,
       gender,
       dateOfBirth,
@@ -10,52 +24,43 @@ export const registerUser = async (req, res) => {
       employeeID,
       position,
       password,
-    } = req.body;
-    try {
-      const existingUser = await User.findOne({ email });
-      if (existingUser)
-        return res.status(400).json({ message: "User already exists" });
-  
-      const user = await User.create({
-        fullName,
-        gender,
-        dateOfBirth,
-        email,
-        employeeID,
-        position,
-        password,
-      });
-  
+    });
+
+    // Generate token
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      message: "User registered successfully",
+      fullName: user.fullName,
+      token, // Return token in response body
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+// Login User
+export const loginUser = async (req, res) => {
+  const { employeeID, password } = req.body;
+  try {
+    const user = await User.findOne({ employeeID });
+    if (user && (await user.matchPassword(password))) {
       // Generate token
       const token = generateToken(user._id);
-  
-      res.status(201).json({
-        message: "User registered successfully",
+
+      res.status(200).json({
         fullName: user.fullName,
         token, // Return token in response body
       });
-    } catch (error) {
-      res.status(500).json({ message: "Server error", error });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
     }
-  };
-  
-  // Login User
-  export const loginUser = async (req, res) => {
-    const { employeeID, password } = req.body;
-    try {
-      const user = await User.findOne({ employeeID });
-      if (user && (await user.matchPassword(password))) {
-        // Generate token
-        const token = generateToken(user._id);
-  
-        res.status(200).json({
-          fullName: user.fullName,
-          token, // Return token in response body
-        });
-      } else {
-        res.status(401).json({ message: "Invalid credentials" });
-      }
-    } catch (error) {
-      res.status(500).json({ message: "Server error", error });
-    }
-  };
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+// Logout User
+export const logoutUser = (req, res) => {
+  res.clearCookie("token").status(200).json({ message: "Logout Successful" });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express"; // Use `import` for express
-import { registerUser, loginUser } from "../controllers/userController.js"; // `import` for controllers
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/userController.js"; // `import` for controllers
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 export default router; // Use `export default` for the router in ES Modules
